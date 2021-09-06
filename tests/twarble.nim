ee75f7e35ffd672df9fe29189942726f4836f934
RattleyCooper <6770988+RattleyCooper@users.noreply.github.com>
2021-09-06 16:55:19 -0700

    Fix assert in test

--- tests/twarble.nim
+++ tests/twarble.nim
@@ -6,11 +6,13 @@ suite "warble":
   var injectedPayload: seq[uint8]
   test "inject":
     echo "Injecting payload..."
+    
     injectedPayload = inject("warble/test-files/test3.png", "warble/test-files/warble.png", "warble/test-files/test3-inj.png")
-    assert(injectedPayload.len < profileImage("warble/test-files/test3.png")-TermBytes.len)
+    assert(injectedPayload.len < profileImage("warble/test-files/test3.png"))
 
   test "extract":
     echo "Extracting payload..."
+
     var payload = extract("warble/test-files/test3-inj.png", "warble/test-files/warble-out.png")
     assert(payload.len == 2360)
     assert(injectedPayload == payload)
